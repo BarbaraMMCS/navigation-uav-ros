221c1cc501875eef0bc618e5755d7c195e9a4f62
barbara <barbarasymeonc@gmail.com>
2020-04-12 21:45:43 +0200

    images

---
+++ Image converter.py
@@ -0,0 +1,33 @@
+import cv2
+
+
+image = cv2.imread("ROS_diagram.png", cv2.IMREAD_GRAYSCALE)
+
+# Image size
+shape = image.shape
+print(shape)
+width, height = shape
+
+# image resize
+
+factor = 0.20
+width_r, height_r = width*factor, height*factor
+print(width_r, height_r)
+dim = (int(width_r), int(height_r))
+
+image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+
+while True:
+
+    cv2.imshow("ROS_diagram.png", image)
+
+    # Quit Esc and timing
+    k = cv2.waitKey(100) & 0xFF
+    if k == 27:  # esc
+        break
+
+    # press "s" to save
+    if k == ord("s"):
+        cv2.imwrite("images/ROS_diagram_1.png", image)
+
+cv2.destroyAllWindows()
--- target.py
+++ target.py
@@ -1,6 +1,11 @@
 import cv2
 import numpy as np
 
+
+def nothing(x):
+    pass
+
+
 # Load image
 image = cv2.imread("/home/barbara/Pictures/NikonDF_0026.jpg")
 
@@ -16,54 +21,43 @@ green_rgb = (0, 255, 0)
 violet_rgb = (170, 0, 170)
 yellow_rgb = (0, 170, 170)
 
-# Shapes
-cv2.line(image, (3200, 2000), (4200, 2000), green_rgb, 2)
-cv2.circle(image, (3700, 1750), 10, red_rgb, -1)
-cv2.ellipse(image, (3700, 2000), (500, 300), 180, 180, 0, green_rgb, 3)
-points = np.array([[[2000, 1000], [2000, 1250], [1300, 1200], [2300, 1400]]], np.int32)
-cv2.polylines(image, [points], True, yellow_rgb, thickness=2)
-cv2.circle(image, (1500, 2400), 10, blue_rgb, -1)
+while True:
 
-# Text
-font = cv2.FONT_HERSHEY_TRIPLEX
-cv2.putText(image, "pyramid rock", (3200, 1600), font, 4, violet_rgb)
+    # Shapes
+    cv2.line(image, (3200, 2000), (4200, 2000), blue_rgb, 2)
+    cv2.circle(image, (3700, 1750), 10, red_rgb, -1)
+    cv2.ellipse(image, (3700, 2000), (500, 300), 180, 180, 0, green_rgb, 3)
+    points = np.array([[[2000, 1000], [2000, 1250], [1300, 1200], [2300, 1400]]], np.int32)
+    cv2.polylines(image, [points], True, yellow_rgb, thickness=2)
 
-# Region of interest
-# row, column, chanel = shape
-pt1 = (1200, 2200)
-pt2 = (1800, 2600)
-pt1_x, pt1_y = pt1
-pt2_x, pt2_y = pt2
-cv2.rectangle(image, pt1, pt2, white_rgb, 3)
-roi = image[pt1_y:pt2_y, pt1_x:pt2_x] # ratio is 2:3 --> [1:row, 1.5:column]
+    # Text
+    font = cv2.FONT_HERSHEY_TRIPLEX
+    cv2.putText(image, "pyramid rock", (3200, 1600), font, 4, violet_rgb)
 
-# image resize after so shapes keeps position
-width = 720
-height = 480
-dim = (width, height)
-image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-roi = cv2.resize(roi, dim, interpolation=cv2.INTER_AREA)
+    # Region of interest
+    # row, column, chanel = shape
+    pt1 = (1200, 2200)
+    pt2 = (1800, 2600)
+    pt1_x, pt1_y = pt1
+    pt2_x, pt2_y = pt2
+    cv2.rectangle(image, pt1, pt2, white_rgb, 3)
+    roi = image[pt1_y:pt2_y, pt1_x:pt2_x] # ratio is 2:3 --> [1:row, 1.5:column]
 
-row, column, chanel = image.shape
+    # image resize after drawing
+    width = 720
+    height = 480
+    dim = (width, height)
+    image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+    roi = cv2.resize(roi, dim, interpolation=cv2.INTER_AREA)
 
-scaled_image = cv2.resize(roi, None, fx=1/2, fy=1/2)
+    # window
+    cv2.imshow("pyramid_rock", image)
+    cv2.imshow("roi", roi)
 
-cv2.imwrite("image.png", image)
-image = cv2.imread("/home/barbara/Pictures/image.jpg")
-
-matrix = np.float32([[1, 0, 50], [1, 0, 50]])
-translation_img = cv2.warpAffine(roi, matrix, (column, row))
-
-print("Height: ", row)
-print("Width: ", column)
-
-
-# window
-cv2.imshow("pyramid_rock", image)
-cv2.imshow("ROI", scaled_image)
-cv2.imshow("translation_img", translation_img)
-
+    # Quit Esc and timing
+    k = cv2.waitKey(0) & 0xFF
+    if k == 27:  # esc
+        break
 
 # quit
-cv2.waitKey(0) & 0xFF == ord("q")
 cv2.destroyAllWindows()
