345b7d8481503cb8dd4afa3b26a936fc85d12465
barbara <barbarasymeonc@gmail.com>
2020-04-27 19:03:31 +0200

    pkg update

--- ros_opencv_pkg/src/node.py
+++ ros_opencv_pkg/src/node.py
@@ -1,51 +1,28 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msg import String
-from ros_opencv_pkg.msg import custom_msg
+from std_msgs.msg import String
+
 
 
 def publisher():
     pub = rospy.Publisher('message', String, queue_size=10)
-    pub = rospy.Publisher('custom message', custom_msg, queue_size=10)
-
     rate = rospy.Rate(10)  #10HZ
 
-    custom_msg_to_pub = custom_msg()
-
-    counter = 0
 
     while not rospy.is_shutdown():
         str = "Publishing message %s" % rospy.get_time()
-        custom_str = "Publishing custom message %d" % counter
-        counter +=1
-
-        custom_msg_to_pub.data = custom_str
-        custom_msg_to_pub.counter = counter
-
         pub.publish(str)
-        pub.publish(custom_str)
-
         rospy.loginfo(str)
-        rospy.loginfo(custom_str)
-
         rate.sleep()
 
 
-def callback_data(data):
+def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "received: %s", data.data)
 
 
-def callback_msg(custom_str):
-    str_received = message.data
-    counter_received = message.counter
-    rospy.loginfo("received: %d" % counter_received)
-
-
 def subscriber():
-    sub = rospy.Subscriber('message', String, callback_data)
-    sub = rospy.Subscriber('custom message', custom_msg, callback_msg)
-
+    sub = rospy.Subscriber('message', String, callback)
     rospy.spin()
 
 
