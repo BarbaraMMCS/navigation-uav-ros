e0c9cde6b0f4768250f820f00d9d3a48afb7a22a
barbara <barbarasymeonc@gmail.com>
2020-04-28 11:05:54 +0200

    node update

---
+++ ros_opencv_pkg/src/video.cpp
@@ -0,0 +1,60 @@
+#include <ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+class cam_test{
+    public:
+        cam_test(){
+            videoCapture cap(CV_CAP_ANY);
+                if (!cap.isOpened())
+                {
+                    cout << "Cannot open video" << endl;
+                }
+
+                double dwidht = cap.get((CV_CAP_PROP_FRAME_WIDTH);
+                double dheight = cap.get((CV_CAP_PROP_FRAME_HEIGHT);
+
+                cout << "Frame size: " << dwidht << " x " << dheight <<  endl;
+
+                namedWindow("MyVideo", CV_WINDOW_AUTOSIZE);
+
+                while (1)
+                {
+                    Mat frame;
+
+                    bool bsuccess = cap.read(frame);
+                        if (!bSuccess)
+                        {
+                            cout <<< "Cannot read camera stream" << endl;
+                            break;
+                        }
+                        imshow("MyVideo", frame);
+
+                        if (waitKey(30) == 27) // esc to exit
+                        {
+                            break;
+                        }
+                    }
+            }
+            -cam_test(){
+                cvDestroyWindow("Camera_Output")
+            }
+        }
+
+};
+
+int main(int argc, char ***argv)
+{
+    ros::init(argc, argv, "video.cpp");
+    cam_test cam_object;
+
+    ROS_INFO("video works!")
+}
+
+
+
+
