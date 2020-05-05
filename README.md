eb96dde5f2798fecb4d2ab821addf499f3dd26df
barbara <barbarasymeonc@gmail.com>
2020-05-05 17:50:43 +0200

    node update

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
           
 <img src="images/pyramid_rock.jpg" width="1080">
  
+
+
+Package needed are : 
+
+vision_opencv : http://wiki.ros.org/vision_opencv
+
+usb_cam : http://wiki.ros.org/usb_cam
+
+cv_camera : http://wiki.ros.org/cv_camera
--- image/src/image.py
+++ image/src/image.py
@@ -1,48 +1,61 @@
 #!/usr/bin/env python
 
-import rospy
-import cv2
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
+"""
+description
+usage example
+parameters
+"""
+
 import numpy as np
+import cv2 as cv
+
+import sys
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
 
 
 def subscriber():
     rospy.Subscriber("image", Image, callback, queue_size=1)
-    rospy.loginfo("Subscriber is listening")	
-	
-
-def callback(data):
-   img = bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
-   cv2.imshow("image", img)
-   if cv2.waitKey(25) & 0xFF == ord('q'):
-	cv2.destroyAllWindows()
-
+    rospy.loginfo("Subscriber is listening")
 
 
 def publisher():
     pub = rospy.Publisher("image", Image, queue_size=10)
-    rate = rospy.Rate(10)  #10HZ
-    rospy.loginfo("Publisher is publishing")		
+    rate = rospy.Rate(10)  # 10HZ
+    rospy.loginfo("Publisher is publishing")
 
     while not rospy.is_shutdown():
-        msg = bridge.cv2_to_imgmsg(data, encoding="passthrough")
+        msg = br.cv2_to_imgmsg(data, encoding="passthrough")
         pub.publish(msg)
         rate.sleep()
 
 
-if __name__ == '__main__':
-
+def callback(data):
     try:
-        bridge = CvBridge()
-        data = cv2.imread("/home/barbara/Pictures/pyramid_rock.jpg")
+        img = br.imgmsg_to_cv2(data, desired_encoding="passthrough")
+    except CvBridgeError as error:
+        print(error)
+    cv.imshow("image", img)
+    cv.waitKey(0)
 
-        rospy.init_node("image", anonymous= True)
+
+
+if __name__ == '__main__':
+    br = CvBridge()
+    data = cv.imread("/home/barbara/Pictures/pyramid_rock.jpg")
+    if (data.any()):
+        rospy.init_node("image", anonymous=True)
         subscriber()
         publisher()
+    else:
+        print("Could not open image")
+    try:
         rospy.spin()
-
     except KeyboardInterrupt:
         print("Shutting down")
 
-    cv2.destroyAllWindows()
+    cv.destroyAllWindows()
+
