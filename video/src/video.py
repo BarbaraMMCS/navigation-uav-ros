1fd34da8f6c56912ead4081e3481c1b533788323
barbara <barbarasymeonc@gmail.com>
2020-05-07 16:17:44 +0200

    video update

--- video/src/video.py
+++ video/src/video.py
@@ -2,17 +2,15 @@
 
 import cv2
 import rospy
-from cv_bridge import CvBridge,CvBridgeError
+from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
-
 
 video_path = "/home/barbara/Videos/Drone/DJI_0004.MOV"
 bridge = CvBridge()
-cap = cv2.VideoCapture(video_path)			
+cap = cv2.VideoCapture(video_path)
 
 
 def publisher():
-
     pub = rospy.Publisher("/image_raw", Image, queue_size=10)
     rate = rospy.Rate(10)
     rospy.loginfo("Publisher is starting")
@@ -26,10 +24,10 @@ def publisher():
 
 
 def callback(data):
-
     image = bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
-    _, frame = cap.read()
-    cv2.imshow('frame', frame)
+
+    cv2.imshow("video", image)
+
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
 
@@ -46,5 +44,6 @@ def main():
     rospy.spin()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
+
