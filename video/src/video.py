efe3afc3c0a01f4b7f1cf7ae2d7490ea9e47db70
barbara <barbarasymeonc@gmail.com>
2020-05-07 16:03:11 +0200

    readme update

--- video/src/video.py
+++ video/src/video.py
@@ -3,7 +3,7 @@ import cv2
 import rospy
 from cv_bridge import CvBridge,CvBridgeError
 from sensor_msgs.msg import Image
-from mydia import Videos
+
 
 
 video_path = "/home/barbara/Videos/Drone/DJI_0004.MOV"
@@ -43,21 +43,9 @@ def listener():
     rospy.loginfo("Subscriber is starting")
 		
     reader = Videos(num_frames=120, mode=select_frames)
-    video = reader.read(video_path)  # A video tensor/array
 
 
-def select_frames(total_frames, num_frames, fps, *args):
-    """This function will return the indices of the frames to be captured"""
-    N = 1
-    t = np.arange(total_frames)
-    f = np.arange(num_frames)
-    mask = np.resize(f, total_frames)
 
-    return t[mask < N][:num_frames].tolist()
-
-   # Let's assume that the duration of your video is 120 seconds
-   # and you want 1 frame for each second 
-   # (therefore, setting `num_frames` to 120)
 
 
 
