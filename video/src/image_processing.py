6500b0053f4c766cf54003c61a28ea68bf578b30
barbara <barbarasymeonc@gmail.com>
2020-05-07 16:24:12 +0200

    video update

--- video/src/image_processing.py
+++ video/src/image_processing.py
@@ -8,8 +8,8 @@ from sensor_msgs.msg import Image
 
 bridge = CvBridge()
 
-def callback(data):
 
+def callback(data):
 	image = bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
 
 	cv2.imshow("video", image)
@@ -17,11 +17,13 @@ def callback(data):
 	if cv2.waitKey(25) & 0xFF == ord('q'):
 		cv2.destroyAllWindows()
 
+
 def listener():
 	rospy.init_node("image_processing", anonymous=True)
 	rospy.Subscriber("/image_raw", Image, callback)
 	rospy.loginfo("Subscriber is starting")		
 	rospy.spin()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
 	listener()
