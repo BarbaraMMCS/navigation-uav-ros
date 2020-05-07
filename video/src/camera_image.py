0dcb8c6f40227d428f77bd0d40e40a0b9b174458
barbara <barbarasymeonc@gmail.com>
2020-05-07 16:35:09 +0200

    video update

--- video/src/camera_image.py
+++ video/src/camera_image.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
+
 import cv2
 import rospy
 from cv_bridge import CvBridge,CvBridgeError
 from sensor_msgs.msg import Image
 
-bridge= CvBridge()
-cap = cv2.VideoCapture("/home/barbara/Videos/Drone/DJI_0004.MOV")			
 
-rospy.init_node("camera_image", anonymous=True)
-pub = rospy.Publisher("/image_raw", Image, queue_size=10)
-rate = rospy.Rate(10)
-rospy.loginfo("Publisher is starting")
+bridge = CvBridge()
+cap = cv2.VideoCapture("/home/barbara/Videos/Drone/DJI_0004.MOV")
 
-while cap.isOpened():
 
-	_,frame=cap.read()
-	image = bridge.cv2_to_imgmsg(frame, encoding="passthrough")
-	pub.publish(image)
-	rate.sleep()
-				
-	
-cap.release()
-	
+def publisher():
+    pub = rospy.Publisher("/image_raw", Image, queue_size=10)
+    rate = rospy.Rate(10)
+    rospy.loginfo("Publisher is starting")
+
+    while cap.isOpened():
+        _, frame = cap.read()
+        image = bridge.cv2_to_imgmsg(frame, encoding="passthrough")
+        pub.publish(image)
+        rate.sleep()
+    cap.release()
+
+def main():
+    rospy.init_node("camera_image", anonymous=True)
+    publisher()
+
+if __name__=='__main__':
+    main()
--- video/src/image_processing.py
+++ video/src/image_processing.py
@@ -5,25 +5,24 @@ import cv2
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 
-
 bridge = CvBridge()
 
 
 def callback(data):
-	image = bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
+    image = bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
 
-	cv2.imshow("video", image)
-	
-	if cv2.waitKey(25) & 0xFF == ord('q'):
-		cv2.destroyAllWindows()
+    cv2.imshow("video", image)
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
 
 
 def listener():
-	rospy.init_node("image_processing", anonymous=True)
-	rospy.Subscriber("/image_raw", Image, callback)
-	rospy.loginfo("Subscriber is starting")		
-	rospy.spin()
+    rospy.init_node("image_processing", anonymous=True)
+    rospy.Subscriber("/image_raw", Image, callback)
+    rospy.loginfo("Subscriber is starting")
+    rospy.spin()
 
 
 if __name__ == '__main__':
-	listener()
+    listener()
