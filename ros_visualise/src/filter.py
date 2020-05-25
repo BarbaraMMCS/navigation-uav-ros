9d3d51b3a42ffeb209259f7e32ea4cccc333e975
barbara <barbarasymeonc@gmail.com>
2020-05-25 22:51:56 +0200

    package update

--- ros_visualise/src/filter.py
+++ ros_visualise/src/filter.py
@@ -12,31 +12,30 @@ class Filter:
 
     def __init__(self):
         self.bridge = CvBridge()
+        cv2.namedWindow("video")
+        cv2.createTrackbar("value", "video", 128, 255, self.nothing)
 
     def nothing(self, x):
         pass
 
     def filter(self, frame):
+        value_th = cv2.getTrackbarPos("value", "video")
+        _, th_trunc = cv2.threshold(frame, value_th, 255, cv2.THRESH_TRUNC)
 
-        cv2.namedWindow("video")
-        cv2.createTrackbar("value", "video", 128, 255, self.nothing)
-        while True:
-            value_th = cv2.getTrackbarPos("value", "video")
-            _, th_trunc = cv2.threshold(frame, value_th, 255, cv2.THRESH_TRUNC)
+        cv2.imshow("video", th_trunc)
 
-            cv2.imshow("th_trunc", th_trunc)
-            cv2.imshow("video", frame)
-
-            if cv2.waitKey(15) & 0xFF == ord('q'):
-                cv2.destroyAllWindows()
+        if cv2.waitKey(15) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
 
     def callback(self, data):
         try:
             frame = self.bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
         except CvBridgeError as e:
             print(e)
-
-        self.filter(frame)
+        if frame.any():
+            self.filter(frame)
+        else:
+            pass
 
 
 def callback(data):
@@ -50,7 +49,6 @@ def listener():
 
 def main():
     rospy.init_node("filter", anonymous=True)
-
     while not rospy.is_shutdown():
         data = rospy.wait_for_message('/gray', Image)
         listener()
@@ -62,5 +60,5 @@ if __name__ == '__main__':
     try:
         main()
     except rospy.ROSInterruptException:
-
         pass
+
