ca202545076e60d929096c759d89bee1aef5c29b
barbara <barbarasymeonc@gmail.com>
2020-05-25 19:42:27 +0200

    package update

--- ros_visualise/src/filter.py
+++ ros_visualise/src/filter.py
@@ -6,37 +6,29 @@ import rospy
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
-import numpy as np
-
 
 
 class Filter:
 
     def __init__(self, src=0):
         self.bridge = CvBridge()
-        
 
     def nothing(self, x):
         pass
 
-
     def filter(self, frame):
+
         cv2.namedWindow("video")
         cv2.createTrackbar("value", "video", 128, 255, self.nothing)
         while True:
-
             value_th = cv2.getTrackbarPos("value", "video")
-
             _, th_trunc = cv2.threshold(frame, value_th, 255, cv2.THRESH_TRUNC)
 
             cv2.imshow("th_trunc", th_trunc)
-
             cv2.imshow("video", frame)
-
 
             if cv2.waitKey(15) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
-
 
     def callback(self, data):
         try:
@@ -47,22 +39,17 @@ class Filter:
         frame = self.filter(frame)
 
 
-
-        
-
 cf = None
 run = Filter()
 
 
 def callback(data):
-    run.callback(data)    
-
+    run.callback(data)
 
 
 def listener():
     rospy.Subscriber('/gray', Image, callback)
     rospy.loginfo("Subscriber is starting")
-
 
 
 def main():
@@ -74,12 +61,9 @@ def main():
         rospy.spin()
 
 
-
-
 if __name__ == '__main__':
     try:
         main()
     except rospy.ROSInterruptException:
 
         pass
-
