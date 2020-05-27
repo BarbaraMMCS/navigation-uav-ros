a3f0106a1749f311803578988c936a9c3f79bebc
barbara <barbarasymeonc@gmail.com>
2020-05-27 23:15:12 +0200

    package update

--- ros_visualise/scripts/filters.py
+++ ros_visualise/scripts/filters.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# video subscriber node b
+# node b
 
 import rospy
 import cv2
@@ -12,41 +12,70 @@ class Filter:
 
     def __init__(self):
         self.bridge = CvBridge()
-        cv2.namedWindow("filters")
-        cv2.createTrackbar("Threshold", "filters", 128, 255, self.nothing)
-        cv2.createTrackbar("Gray", "filters", 1, 1, self.nothing)
+        self.window = "filters"
+        cv2.namedWindow(self.window)
+        self.sliders("Threshold", a=128, b=255)
+        self.sliders("Gray")
+
+        # slider and callback
+    def sliders(self, name, a=1, b=1, window=None, call=None):
+        if window is None:
+            window = self.window
+        if call is None:
+            call = self.nothing
+        cv2.createTrackbar(name, window, a, b, call)
 
     def nothing(self, x):
         pass
 
-    def filter(self, data):
-        value_th = cv2.getTrackbarPos("Threshold", "filters")
+        # filters
+    def filters(self, data):
+        value_th = cv2.getTrackbarPos("Threshold", self.window)
         _, data = cv2.threshold(data, value_th, 255, cv2.THRESH_TRUNC)
-        switch_gray = cv2.getTrackbarPos("Gray", "filters")
-        if switch_gray == 0:
+        switch = cv2.getTrackbarPos("Gray", self.window)
+        if switch == 0:
             pass
         else:
             data = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("filters", data)
-        if cv2.waitKey(15) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
         return data
 
-    def callback(self, data):
+        # msgs conversion from topic subscribed to publish
+    def to_cv2(self, data, encoding="passthrough"):
         try:
-            data = self.bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
+            data = self.bridge.imgmsg_to_cv2(data, desired_encoding=encoding)
         except CvBridgeError as e:
             print(e)
+        return data
+
+    def to_imgmsg(self, data, encoding="passthrough"):
+        try:
+            data = data = self.bridge.cv2_to_imgmsg(data, encoding=encoding)
+        except CvBridgeError as e:
+            print(e)
+        return data
+
+        # filters : gray and threshold
+    def show(self, data, wait=1):
+        cv2.imshow(self.window, data)
+        if cv2.waitKey(wait) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+
+        # publisher
+    def publisher(self, data, rate=30, queue=10):
+        topic_pub = rospy.get_param('~topic_pub')
+        pub = rospy.Publisher(topic_pub, Image, queue_size=queue)
+        pub.publish(data)
+        rate = rospy.Rate(rate)
+        rate.sleep()
+
+        # subscriber callback
+    def callback(self, data):
+        data = self.to_cv2(data)
         if data.any():
-            data = self.filter(data)
-            try:
-                data = self.bridge.cv2_to_imgmsg(data, encoding="passthrough")
-            except CvBridgeError as error:
-                print(error)
-            topic_pub = rospy.get_param('~topic_pub')
-            pub = rospy.Publisher(topic_pub, Image, queue_size=10)
-            rospy.loginfo_once("Publisher is sending data")
-            pub.publish(data)
+            data = self.filters(data)
+            self.show(data)
+            data = self.to_imgmsg(data)
+            self.publisher(data)
         else:
             pass
 
@@ -58,19 +87,18 @@ def callback(data):
 def subscriber():
     topic = rospy.get_param('~topic_sub')
     rospy.Subscriber(topic, Image, callback)
-    rospy.loginfo("Subscriber is starting")
 
 
 def main():
-    rospy.init_node("filters", anonymous=True)
+    rospy.init_node(image.window, anonymous=True)
     while not rospy.is_shutdown():
         subscriber()
         rospy.spin()
 
 
 if __name__ == '__main__':
-    image = Filter()
     try:
+        image = Filter()
         main()
     except rospy.ROSInterruptException:
         pass
